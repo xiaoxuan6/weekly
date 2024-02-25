0b4cb2970202070a4a6b4481a58bf0f80bd58e35
xiaoxuan6 <1527736751@qq.com>
2024-02-25 17:56:48 +0800

    feat: Update index.js 适配 cloudflare workers

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
     $('.md-content a').attr('target', '_blank');
     $('.md-source').attr('target', '_blank');
+
+    function checkURL() {
+        let currentURL = window.location.href;
+        let desiredURL = 'https://xiaoxuan6.github.io/weekly';
+
+        if (currentURL.startsWith(desiredURL) === false) {
+            $('img').each(function() {
+                let currentSrc = $(this).attr('src');
+                if (currentSrc.startsWith('/weekly/static')) {
+                    let newSrc = currentSrc.replace('/weekly/static', '/static');
+                    $(this).attr('src', newSrc);
+                }
+            });
+        }
+    }
+
+    checkURL();
 });
