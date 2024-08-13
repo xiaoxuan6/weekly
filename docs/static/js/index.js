39d122705f84a7e888a754d55319264364935dde
xiaoxuan6 <1527736751@qq.com>
2024-08-13 13:37:41 +0800

    feat: Add github 文件加速

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.md-content a').attr('target', '_blank');
     $('.md-source').attr('target', '_blank');
 
@@ -8,7 +8,7 @@ $(document).ready(function() {
         let uri = 'https://mirror.ghproxy.com/https://raw.githubusercontent.com/xiaoxuan6/weekly/main/docs';
 
         if (currentURL.startsWith(desiredURL) === false) {
-            $('img').each(function() {
+            $('img').each(function () {
                 let currentSrc = $(this).attr('src');
                 if (currentSrc.startsWith(uri)) {
                     let newSrc = currentSrc.replace(uri, '');
@@ -16,6 +16,16 @@ $(document).ready(function() {
                 }
             });
         }
+
+        const allLinks = document.querySelectorAll('a');
+
+        allLinks.forEach(link => {
+            if (link.textContent.includes('Github 文件加速')) {
+                link.setAttribute('target', '_blank');
+            } else {
+                console.log('未找到 a 标签')
+            }
+        });
     }
 
     checkURL();
